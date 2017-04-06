80fc5e36a34e280b325e1fcb2e35f6d8111d926c
NEETShonen <p16115@students.cs.unipi.gr>
2017-04-06 19:44:02 +0300

    kill me

--- Training/howtobinarytree.cpp
+++ Training/howtobinarytree.cpp
@@ -2,11 +2,22 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
 struct node
 {
+	
   int key_value;
   node *left;
   node *right;
+  //dedomena ptisis
+  int fcode ,cost,seats,rseats;
+	//  fcode = Flight code cost = ticket cost
+	//  seats = all seats , rseats = reserved seats
+	struct time{
+		int hours;
+		int minutes;
+	}start,reach;
+	char from[2],to[2];
 };
 class btree
 {
@@ -33,6 +44,7 @@ void btree::destroy_tree(node *leaf)
     destroy_tree(leaf->left);
     destroy_tree(leaf->right);
     delete leaf;
+    leaf=NULL;
   }
 }
 int o=0;
@@ -46,6 +58,7 @@ void btree::insert(int key, node *leaf)
     {
       leaf->left=new node;
       leaf->left->key_value=key;
+
       leaf->left->left=NULL;    //Sets the left child of the child node to null
       leaf->left->right=NULL;   //Sets the right child of the child node to null
 	  std::cout<<"New flightcode added left\n";
@@ -58,9 +71,43 @@ void btree::insert(int key, node *leaf)
     if(leaf->right!=NULL)
       insert(key, leaf->right);
     else
-    {
+    {	
+	  int cost,seats,rseats;
+	  //  cost = ticket cost
+	  //  seats = all seats , rseats = reserved seats
+	  struct time{
+		int hours;
+		int minutes;
+	  }start,reach;
+	  char from[2],to[2];
+	  std::cout<<"COST:";
+	  std::cin>>cost;
+	  std::cout<<"SEATS:";      
+	  std::cin>>seats;
+	  std::cout<<"RESERVED SEATS:";
+	  std::cin>>rseats;
+	  std::cout<<"START HOUR:";
+	  std::cin>>start.hours>>start.minutes;
+	  std::cout<<"REACH HOUR:";
+	  std::cin>>reach.hours>>reach.minutes;
+	  std::cout<<"FROM:";
+	  std::cin>>from;
+	  std::cout<<"TO:";
+	  std::cin>>to;//telika kanoun kati alla den to kanoun kala lol >.<
       leaf->right=new node;
       leaf->right->key_value=key;
+      leaf->right->cost=cost;
+      leaf->right->seats=seats;
+      leaf->right->rseats=rseats;
+      leaf->right->start.hours=start.hours;
+      leaf->right->start.minutes=start.minutes;
+      leaf->right->reach.hours=reach.hours;
+      leaf->right->reach.minutes=reach.minutes;
+	  for (int i=0;i!=2;i++){//kapou malakizete edw nomizw
+		leaf->right->from[i]=from[i];
+		leaf->right->to[i]=to[i];
+	  }
+      //end of import 
       leaf->right->left=NULL;  //Sets the left child of the child node to null
       leaf->right->right=NULL; //Sets the right child of the child node to null
 	  std::cout<<"New flightcode added right\n";
@@ -75,8 +122,11 @@ node *btree::search(int key, node *leaf)
 {
   if(leaf!=NULL)
   {
-    if(key==leaf->key_value)
-      return leaf;
+    if(key==leaf->key_value){
+      cout<<"CODE "<<leaf->key_value<<"\nSEATS "<<leaf->seats<<"\nRESERVED SEATS "<<leaf->rseats<<"\nDEPARTURE TIME "<<leaf->start.hours<<":"<<leaf->start.minutes<<"\nDEPARTURE AIRPORT "<<leaf->from<<
+      "\nARRIVAL AIRPORT "<<leaf->to<<endl;
+      cout<<"kati egine ?";
+      return leaf;}
     if(key<leaf->key_value)
       return search(key, leaf->left);
     else
@@ -119,8 +169,8 @@ int main(){
 	}
 	else if(p=='s'){
 	cin>>a;
-	std::cout<<e.search(a);}
-
+	e.search(a);
+	}
 	else if(p=='d'){
 		cin>>a;
 		e.destroy_tree(a);
