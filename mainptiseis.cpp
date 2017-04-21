cf3e1b787bc36fd56798e4d81160f76186ed7adc
NEETShonen <p16115@students.cs.unipi.gr>
2017-04-21 16:59:09 +0300

    HEHEHEHEHEHEHEHE END ME

--- mainptiseis.cpp
+++ mainptiseis.cpp
@@ -13,10 +13,10 @@ char from[4],to[4];
 };//Για τις πτησεις
 
 struct line{
-		string frist,last;
+		string first,last;
 		int key;
 		line *next;
-};//Για την ουρα
+};//Για την ουρα (Τα υπολοιπα τα βαζουμε μετα)
 
 class btreeandline{
     public:
@@ -35,6 +35,11 @@ class btreeandline{
         void addqueue(int key,string first,string last);
         void removequeue(int key,string first, string last);
         void removeall(int key);
+		line *searchqueue(int key,string first,string last);
+
+
+
+
     private:
         node *root;
         node *search(int key,node *leaf);
@@ -45,6 +50,9 @@ class btreeandline{
         int  freeseats(int key,node *leaf);
         void reserved(int key , int a,node *leaf);
         bool isleaf(int key,node *leaf);
+        void addqueue(int key,string first,string last,line *leaf);
+		line *searchqueue(int key,string first,string last,line *leaf);
+		line *lastonline(line *leaf);
         //tbc
 		line *start;
 };
@@ -156,6 +164,34 @@ node *btreeandline::search(int key, node *leaf){//PRIVATE
 
 
 
+node *btreeandline::previous(int key){//PUBLIC
+	if (root!=NULL){
+		return search(key,root);
+	}
+	else{
+		return NULL;
+	}
+}
+node *btreeandline::previous(int key, node *leaf){//PRIVATE
+	if (leaf!=NULL){
+		if ((key==leaf->left->key)||(key==leaf->right->key)){
+			return leaf;
+		}
+		else if (key>leaf->key){
+			search(key,leaf->right);
+		}
+		else if (key<leaf->key){
+			search(key,leaf->left);
+		}	
+	}
+	else{
+	return NULL;
+	}
+}//same error as above :)
+
+
+
+
 void btreeandline::loudsearch(int key){//PUBLIC
 	loudsearch(key,search(key));
 	
@@ -204,7 +240,85 @@ void btreeandline::reserved(int key,int a, node *leaf){//PRIVATE
 	if (freeseats(key)>0){
 		leaf->rseats+=a*1;
 	}
+	else{
+		if ((start==NULL)&&(a==1)){
+			start=new line;
+			cout<<"You are about to add to queue "<<endl;
+			cout<<"GIVE FIRST NAME"<<endl;
+			cin>>start->first;
+			cout<<"GIVE LAST NAME"<<endl;
+			cin>>start->last;
+			start->key=key;
+			start->next=NULL;
+		}
+		else if ((start!=NULL)&&(a==1)){
+			cout<<"You are about to add to queue "<<endl;
+			string lastname,firstname;
+			cout<<"GIVE FIRST NAME"<<endl;
+			cin>>firstname;
+			cout<<"GIVE LAST NAME"<<endl;
+			cin>>lastname;
+			addqueue(key,firstname,lastname);
+		}
+	}
 }
+
+
+line *btreeandline::searchqueue(int key,string first,string last){
+	if (start!=NULL){
+		return searchqueue(key,first,last,start);
+	}else{
+		return NULL;
+	}
+}
+line *btreeandline::searchqueue(int key,string first,string last,line *leaf){
+	if (leaf!=NULL){
+		if (key!=leaf->key){
+			return leaf;
+		}
+		else{
+			return searchqueue(key,first,last,leaf->next);
+		}
+	}
+	else{
+		return NULL;
+	}
+}
+
+
+
+
+line *btreeandline::lastonline(line *leaf){
+	if (leaf->next!=NULL){
+	return lastonline(leaf->next);
+	}
+	else{
+	return leaf->next;	
+	}
+}
+
+
+
+
+
+void btreeandline::addqueue(int key,string first,string last){
+	if (searchqueue(key,first,last)!=NULL){
+		addqueue(key,first,last,lastonline(start));
+	}
+	else{
+		cout<<"YOU ARE ALREADY ON QUEUE"<<endl;
+	}
+}
+void btreeandline::addqueue(int key,string first,string last,line *leaf){
+	leaf= new line;
+	leaf->key=key;
+	leaf->first=first;
+	leaf->last=last;
+}
+
+
+
+
 
 
 
@@ -216,6 +330,7 @@ int main(){
 	cout<<"SELECT :"<<endl;
 	cout<<"A TO ADD FLIGHT"<<endl;
 	cout<<"S TO SEARCH FLIGHT"<<endl;
+	cout<<"R TO RESERVE A SEAT OR ADD TO QUEUE"<<endl;
 	cout<<"X TO QUIT"<<endl;
 	cin>>S;
 	while(S!='X'){
@@ -224,7 +339,12 @@ int main(){
 			cin>>key;
 			a.addflight(key);
 		}
-		if (S=='S'){
+		else if (S=='R'){
+			cout<<"GIVE FLIGHTCODE: ";
+			cin>>key;
+			a.reserved(key,1);
+		}
+		else if (S=='S'){
 			cout<<"GIVE FLIGHTCODE";
 			cin>>key;
 			a.loudsearch(key);
